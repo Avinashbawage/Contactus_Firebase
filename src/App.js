f6d7e425190adebd6d582a86ddcb74c2bac24e0b
Avinash9 <84569613+Avinashbawage@users.noreply.github.com>
2023-08-11 11:59:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import YourComponent from "./contact";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <YourComponent />
+    </div>
+  );
+}
